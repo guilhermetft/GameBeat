043215cb3ff0a235a9746541505221378c01cb86
guilhermetft <guiftf2007@gmail.com>
2026-03-18 20:04:19 -0300

    Add project files.

---
+++ Games.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBeat
+{
+    public class Games
+    {
+        private string nameGame { get; set; }
+        private double hoursPlayed { get; set; }
+        private string genderGame { get; set; }
+        private string plataformThrown { get; set; }
+        private int nota { get; set; }
+        private DateTime startDate { get; set; }
+        private DateTime endDate { get; set; }
+
+        public Games(string nameGame, double hoursPlayed, string genderGame, string plataformThrown, int nota, DateTime startDate, DateTime endDate)
+        {
+            this.nameGame = nameGame;
+            this.hoursPlayed = hoursPlayed;
+            this.genderGame = genderGame;
+            this.plataformThrown = plataformThrown;
+            this.nota = nota;
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
+
+        public Games()
+        {
+            nameGame = null;
+            hoursPlayed = 0;
+            genderGame = null;
+            plataformThrown = null;
+            nota = 0;
+            startDate = DateTime.MinValue;
+            endDate = DateTime.MinValue;
+        }
+
+        public Games(string nameGame, double hoursPlayed, string genderGame, string plataformThrown, int nota, DateTime startDate)
+        {
+            this.nameGame = nameGame;
+            this.hoursPlayed = hoursPlayed;
+            this.genderGame = genderGame;
+            this.plataformThrown = plataformThrown;
+            this.nota = nota;
+            this.startDate = startDate;
+        }
+
+        public Games(string nameGame)
+        {
+            this.nameGame = nameGame;
+        }
+
+        public string GetNameGame()
+        {
+            return nameGame;
+        }
+
+        public void SetNameGame(string nome)
+        {
+            nameGame = nome;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome do jogo: {nameGame}\nHoras jogadas: {hoursPlayed}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace GameBeat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Games game;
+
+            Console.WriteLine("Nome do jogo: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Horas jogadas: ");
+            double horas = double.Parse(Console.ReadLine());
+            Console.WriteLine("Gênero do jogo: ");
+            string genero = Console.ReadLine();
+            Console.WriteLine("Plataforma jogada: ");
+            string plataforma = Console.ReadLine();
+            Console.WriteLine("Nota do jogo: ");
+            int nota = int.Parse(Console.ReadLine());
+            Console.WriteLine("Data de inicio: ");
+            DateTime datainicio = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Data em que zerou: ");
+            DateTime datafim = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Platinado ou 100%: ");
+            string platina = Console.ReadLine();
+
+            game = new Games();
+
+            Console.WriteLine();
+            game.SetNameGame(nome);
+
+            Console.WriteLine(game);
+        }
+    }
+}
